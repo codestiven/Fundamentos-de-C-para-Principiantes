2d4a09a6d520af83e7ccb26d80ce07cd139ac89f
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-05 15:29:38 -0400

    4. Hola Mundo

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Fundamentos_de_C__para_Principiantes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello word!");
+            Console.WriteLine("esta es otra linea de texto");
         }
     }
 }
