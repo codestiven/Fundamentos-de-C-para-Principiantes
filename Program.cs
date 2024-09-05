4c22b8476a3f07edcd474e7eca8df1dd248cad7a
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-05 16:55:09 -0400

    6. Tipos predefinidos

--- Program.cs
+++ Program.cs
@@ -10,15 +10,36 @@ namespace Fundamentos_de_C__para_Principiantes
     {
         static void Main(string[] args)
         {
-            int numero = 10;
-            int resultado = numero * 5;
-            Console.WriteLine("{0}", resultado);
+            // Tipos enteros
+            // System.Int32
+            int entero = 100; // 32 bits
+            // System.Int16
+            short s = 140; // 16 bits
+            // System.Byte
+            byte b = 10; // 8 bits
+            // System.Int64
+            long largo = 1234124312; // 64 bits
 
-            int suma = 8 + 20;
-            Console.WriteLine("{0}", suma);
+            Console.WriteLine("int: {1}, short: {0}", s, entero);
 
-            string cadena = "Este es un Texto";
-            Console.WriteLine("{0}", cadena);
+            // Tipos flotantes
+            // System.Single
+            float f = 123.5f;
+            // System.Double
+            double d = 3.14159;
+            // System.Decimal utilizado para operaciones financieras
+            decimal dec = 129.99m;
+
+            Console.WriteLine("float: {0}, dec: {1}", f, dec);
+
+            // Otros
+            // System.Char
+            char c = 'E';
+            // System.Boolean representa un valor verdadero o falso
+            bool booleano = true;
+
+            Console.WriteLine("char: {0}", c);
+            Console.WriteLine("bool: {0}", booleano);
         }
     }
 }
