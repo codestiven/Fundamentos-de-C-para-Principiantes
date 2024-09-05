a959dca535522f75418063cde4856c0a25654a8b
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-05 15:24:13 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos_de_C__para_Principiantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
