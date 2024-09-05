85aba1e2d0dbce72bf5d263c0afac1e8f26a4de3
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-05 17:13:37 -0400

    8. Sentencias Condicionales

--- Program.cs
+++ Program.cs
@@ -10,24 +10,65 @@ namespace Fundamentos_de_C__para_Principiantes
     {
         static void Main(string[] args)
         {
-            int a = 30;
-            int b = 11;
-            // operador aritmeticos
-            Console.WriteLine(a + b);
-            Console.WriteLine(a - b);
-            Console.WriteLine(a * b);
-            Console.WriteLine(a / b);   
-            Console.WriteLine(a % b); // modulo
+            // If
+            int num = 10;
+            if (num > 5)
+            {
+                Console.WriteLine("num es mayor que 5");
+            }
 
-            //operdores unitarios
-            a++; // a = a + 1
-            a--; // a = a - 1
-            // operadores de comparacion
-            Console.WriteLine(a > b);
-            Console.WriteLine(a < b);
-            Console.WriteLine(a == b);
-            Console.WriteLine(a >= b);
-            Console.WriteLine(a <= b);
+            // Condicional Compuesta
+            string user = "user1";
+            string pass = "hola123";
+            if (user == "user1" && pass == "hola123")
+            {
+                Console.WriteLine("Acceso correcto");
+            }
+
+            // If - Else
+            int edad = 13;
+            if (edad >= 18)
+            {
+                Console.WriteLine("Mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Menor de edad");
+            }
+
+            // If - Else If
+            int a = 8;
+            int b = 8;
+            if (a > b)
+            {
+                Console.WriteLine("{0} > {1}", a, b);
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("{0} < {1}", a, b);
+            }
+            else
+            {
+                Console.WriteLine("{0} == {1}", a, b);
+            }
+
+            // Switch
+            string codigo = "A45";
+            switch (codigo)
+            {
+                case "A1":
+                    Console.WriteLine("Producto 1");
+                    break;
+                case "A2":
+                    Console.WriteLine("Producto 2");
+                    break;
+                case "A3":
+                    Console.WriteLine("Producto 3");
+                    break;
+                default:
+                    Console.WriteLine("Producto no encontrado");
+                    break;
+            }
         }
     }
 }
