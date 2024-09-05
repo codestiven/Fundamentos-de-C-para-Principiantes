6f7b8bcb305dfedbaa0d5c911ee197bcdbb9d8fe
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-05 17:34:13 -0400

    10. Ciclos de repetición (parte 2)

--- Program.cs
+++ Program.cs
@@ -10,28 +10,28 @@ namespace Fundamentos_de_C__para_Principiantes
     {
         static void Main(string[] args)
         {
-            //Ciclo For
-            Console.WriteLine("usando el ciclo For");
-            for (int i = 0; i < 50; i++) 
+            Console.WriteLine("\n usando el break");
+            for (int i = 0; i < 50; i++)
             {
-             Console.WriteLine("Valor de el contado i : {0}" , i);
+                if (i == 20)
+                {
+                    break;
+                }
+                Console.WriteLine("Valor de el contado i : {0}", i);
             }
-            //Ciclo While
-            Console.WriteLine("\n usando el ciclo while");
-            int n = 0;
-            while (n < 50)
+            Console.WriteLine("\n usando el Continue");
+            for (int i = 0; i < 50; i++)
             {
-                Console.WriteLine("Valor de el contador i : {0}" , n);
-                n++;
+                if(i % 2 == 0)
+                {
+                    continue;
+                }
+                if (i == 20)
+                {
+                    break;
+                }
+                Console.WriteLine("Valor de el contado i : {0}", i);
             }
-            //Ciclo Do - while
-            int c = 0;
-            Console.WriteLine("\n usando el ciclo Do - while");
-            do
-            {
-                Console.WriteLine("Valor de el contador i : {0}", c);
-                c++;
-            }while (c < 50);
         }
     }
 }
