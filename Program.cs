4f011a10746e654eafb93f91ca3821be3c1ea487
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-22 21:45:52 -0400

    14. Clases

--- Program.cs
+++ Program.cs
@@ -10,25 +10,19 @@ namespace Fundamentos_de_C__para_Principiantes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("reto tabla de multiplicar");
-            int multiplicando = 0;
-            int multiplicador = 0;
-            Console.Write("hasta donde deseas tu tabla : ");
-            int count = int.Parse(Console.ReadLine());
-            while (multiplicador <= count)
-            {
-                Console.WriteLine("\ntabla de el " + multiplicador + "\n");
-                for (multiplicando = 0; multiplicando <= 12; multiplicando++)
-                {
-                    Console.WriteLine("{0} + {1} = {2}" , multiplicando , multiplicador , multiplicando * multiplicador);
+            //crear un objeto de tipo rectangulo
+            Rectangulo rectangulo = new Rectangulo();
+            rectangulo.Altura = 5;
+            rectangulo.Base = 5;
 
-                }
-                multiplicador++;
-            
+            Console.WriteLine(rectangulo.CalcularArea());
+
+            // sintaxi de inicializacion 
+            Rectangulo rectangulo1 = new Rectangulo { Altura=3 ,Base = 8};
+            Console.WriteLine(rectangulo1.CalcularArea());
 
 
 
-            }
         }
     }
 }
---
+++ Rectangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos_de_C__para_Principiantes
+{
+    internal class Rectangulo
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+
+        public int CalcularArea() { 
+        return Base * Altura;
+        }
+    }
+}
