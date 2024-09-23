94b184c0636b2f837553e9f324733487674ed557
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-22 22:19:29 -0400

    15. Constructores

---
+++ CuentaBancaria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos_de_C__para_Principiantes
+{
+    internal class CuentaBancaria
+    {
+        public string NoCuenta { get; set; }
+        public string Usuario { get; set; }
+        public decimal Saldo { get; set; }
+
+
+
+        public CuentaBancaria(string NoCuenta , String Usuario , decimal Saldo ) 
+        {
+            NoCuenta = NoCuenta;
+            Usuario = Usuario;
+            Saldo = Saldo;
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,18 +10,9 @@ namespace Fundamentos_de_C__para_Principiantes
     {
         static void Main(string[] args)
         {
-            //crear un objeto de tipo rectangulo
-            Rectangulo rectangulo = new Rectangulo();
-            rectangulo.Altura = 5;
-            rectangulo.Base = 5;
 
-            Console.WriteLine(rectangulo.CalcularArea());
-
-            // sintaxi de inicializacion 
-            Rectangulo rectangulo1 = new Rectangulo { Altura=3 ,Base = 8};
-            Console.WriteLine(rectangulo1.CalcularArea());
-
-
+            CuentaBancaria cuenta = new CuentaBancaria("000000000" , "Stiven" , 15);
+            
 
         }
     }
