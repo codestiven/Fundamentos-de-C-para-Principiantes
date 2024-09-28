b4dbe6d43f1be0ea1c6b5b70a549aa2a6f84b732
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-27 20:14:37 -0400

    21. Uso de Namespaces

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Fundamentos_de_C__para_Principiantes.Modelos;
+using Fundamentos_de_C__para_Principiantes.Servicios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +12,36 @@ namespace Fundamentos_de_C__para_Principiantes
     {
         static void Main(string[] args)
         {
+            // Creando una instancia de Persona desde el namespace Modelos
+            Persona persona = new Persona();
+            persona.Nombre = "Stiven";
+            persona.Edad = 25;
 
-            CuentaBancaria cuenta = new CuentaBancaria("000000000" , "Stiven" );
+            // Usando el servicio para mostrar la información de la persona
+            PersonaService personaService = new PersonaService();
+            personaService.MostrarInformacion(persona);
+        }
+    }
+}
 
-    
+// Namespace para el modelo Persona
+namespace Fundamentos_de_C__para_Principiantes.Modelos
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
 
-            CuentaBancaria.NewBanco("BHD");
-
-
-
-
-
+// Namespace para el servicio que maneja lógica relacionada con Persona
+namespace Fundamentos_de_C__para_Principiantes.Servicios
+{
+    public class PersonaService
+    {
+        public void MostrarInformacion(Persona persona)
+        {
+            Console.WriteLine($"Nombre: {persona.Nombre}, Edad: {persona.Edad}");
         }
     }
 }
